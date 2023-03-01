6c2900de76d1ad95dea62ba902b1dc22d00b60f7
Marcos Andrade <mvpdeandrade@hotmail.com>
2023-03-01 15:53:49 -0300

    Adicionar arquivos de projeto.

---
+++ Queues and Stacks Overview/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Queues_and_Stacks_Overview
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+Stack(Pilha):
+. Empilha os dados( ou seja, os dados só podem ser removidos/adicionados do topo)
+. First come last out ---/ou/--- last in first out
+
+Utilidades de uma pilha:
+. Inverter dados
+. Botão de voltar do browser
+. Undo/Redo buttons (desfazer e refazer)
+
+Operações:
+. Push(Object obj) -> adicionar dados ao topo da fila
+. Object Pop() -> remove dados do topo da fila
+. Object Peek() -> retorna os dados do objeto no topo da fila sem removê-los
+
+
+
+--------------------------------------------------------------------------------------
+
+
+
+Queues(Filas):
+. Adiciona dados por trás e remove pela frente
+. Importante quando a ordem em que os dados chegam é importante
+. Não dá para acessar elementos no meio da fila (Sistema FIFO)
+. FIFO: First in first out
+
+Utilidades Práticas de uma fila
+. Requerimentos do sistema operacional
+. Gerenciar solicitações na web (controle de congestionamento)
+. Entrada de filas nos video games (entrada do usuário para fazer o combo)
+
+Operações:
+. Enqueue(Object obj) -> adiciona dados ao final da fila
+. Object Dequeue() -> remove dados da frente da fila
+. Object Peek() -> retorna os dados do objeto na frente da fila sem remove-lô*/
+
+
+        }
+    }
+}
